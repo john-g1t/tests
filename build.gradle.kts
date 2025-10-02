ba8ea6758df8d973d8b53344a51d182cdde1a9a0
john-g1t <skorniakovschool@gmail.com>
2025-10-02 03:37:35 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "com.github.john_g1t"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "com.github.john_g1t.Main"
+}
